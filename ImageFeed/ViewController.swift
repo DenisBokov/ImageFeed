ffa6a0e4ae1f61732d8859415e5998a0e5cde94f
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2025-10-06 14:49:36 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Denis Bokov on 06.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
