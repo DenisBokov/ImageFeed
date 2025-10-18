31310475ee45da31793cb0f16a199d16b8b20edc
BokovDenis <bokovdenis@outlook.com>
2025-10-18 14:54:26 +0300

    Создан экран профиля пользователя

---
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Denis Bokov on 18.10.2025.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
