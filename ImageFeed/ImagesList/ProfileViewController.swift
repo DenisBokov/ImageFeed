afe463af383c55ee9cb2ae20c205d8eb4a194f1c
BokovDenis <bokovdenis@outlook.com>
2025-10-18 16:12:28 +0300

    Созданы констрейнты

--- ImageFeed/ImagesList/ProfileViewController.swift
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var profileImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
