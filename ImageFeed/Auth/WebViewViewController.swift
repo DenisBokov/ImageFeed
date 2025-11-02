57fd85baacd38f28b1da6397ef92bc45ceec9188
BokovDenis <bokovdenis@outlook.com>
2025-11-02 20:36:06 +0300

    Создан контроллер для входа в приложение

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Denis Bokov on 02.11.2025.
+//
+
+import UIKit
+
+final class WebViewViewController: UIViewController {
+    
+    
+}
