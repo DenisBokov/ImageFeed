4c2f6176355563d8b87004631348394e464059ae
BokovDenis <bokovdenis@outlook.com>
2025-11-02 20:37:14 +0300

    Создан контроллер для авторизации

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Denis Bokov on 02.11.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    private let indicatorView: String = "ShowWebView"
+}
