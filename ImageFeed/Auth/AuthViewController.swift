476b7882f45ca13e3f38833819797db2adb1daef
BokovDenis <bokovdenis@outlook.com>
2025-11-03 11:35:39 +0300

    Добавлена страница авторизации в unsplash через Web View

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -10,4 +10,15 @@ import UIKit
 final class AuthViewController: UIViewController {
     
     private let indicatorView: String = "ShowWebView"
+    
+    override func viewDidLoad() {
+        configureBackButton()
+    }
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "BackwardButton") // 1
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "BackwardButton") // 2
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil) // 3
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black") // 4
+    }
 }
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -6,8 +6,37 @@
 //
 
 import UIKit
+import WebKit
 
 final class WebViewViewController: UIViewController {
+    enum WebViewConstants {
+        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
     
+    @IBOutlet private var webView: WKWebView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadAuthView()
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else { // 1
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),          //2
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),     //3
+            URLQueryItem(name: "response_type", value: "code"),                   //4
+            URLQueryItem(name: "scope", value: Constants.accessScope)             //5
+        ]
+        
+        guard let url = urlComponents.url else {                                 //6
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
