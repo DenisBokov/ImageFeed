7bdf41cc1d55ca921f2dbcebc19c710d1dd17a54
BokovDenis <bokovdenis@outlook.com>
2025-12-23 20:11:25 +0300

    test(testViewControllerCallsViewDidLoad, testPresenterCallsLoadRequest): Создать тесты на связь контроллера и презентера, вызова presenter.viewDidLoad()
    Тесты созданы

---
+++ ImageFeedTests/WebViewTests.swift
@@ -0,0 +1,77 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Denis Bokov on 22.12.2025.
+//
+
+@testable import ImageFeed
+import XCTest
+
+final class ImageFeedTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+           viewController.presenter = presenter
+           presenter.view = viewController
+        
+        //when
+         _ = viewController.view
+         
+         //then
+         XCTAssertTrue(presenter.viewDidLoadCalled) //behaviour verification
+    }
+    
+    func testPresenterCallsLoadRequest() {
+        //given
+        let viewController = WebViewViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        presenter.viewDidLoad()
+        
+        //then
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+}
+
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+    
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+}
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+
+    var loadRequestCalled: Bool = false
+
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+
+    func setProgressValue(_ newValue: Float) {
+
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+
+    }
+}
