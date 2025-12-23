b5794e5325af6e34d4abf5ecbffe5736bd34aff7
BokovDenis <bokovdenis@outlook.com>
2025-12-23 20:55:08 +0300

    test(testAuthHelperAuthURL, testCodeFromURL): Создать тесты хелпера
    Созданы и протестировано получение ссылки авторизации, получение кода URL

--- ImageFeedTests/WebViewTests.swift
+++ ImageFeedTests/WebViewTests.swift
@@ -63,7 +63,41 @@ final class ImageFeedTests: XCTestCase {
         
         //then
         XCTAssertTrue(shouldHideProgress)
-    } 
+    }
+    
+    func testAuthHelperAuthURL() {
+        //given
+        let configuration = AuthConfiguration.standard
+        let authHelper = AuthHelper(configuration: configuration)
+        
+        //when
+        let url = authHelper.authURL()
+
+        guard let urlString = url?.absoluteString else {
+            XCTFail("Auth URL is nil")
+            return
+        }
+
+        //then
+        XCTAssertTrue(urlString.contains(configuration.authURLString))
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
+    }
+    
+    func testCodeFromURL() {
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native")!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "code", value: "test code")
+        ]
+        let url = urlComponents.url!
+        let authHelper = AuthHelper()
+        
+        let code = authHelper.code(from: url)
+        
+        XCTAssertEqual(code, "test code")
+    }
 }
 
 
