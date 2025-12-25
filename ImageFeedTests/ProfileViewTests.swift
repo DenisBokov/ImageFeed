8561ab6040b0a70e69e92b2fd51a1b03fdc4559f
BokovDenis <bokovdenis@outlook.com>
2025-12-25 21:25:52 +0300

    test, refactor(testProfileViewControllerUpdateAvatarCalled, testProfileViewControllerUpdateProfile, testProfileViewControllerSetDataProfile): Покрыть тестами ProfileViewController
    Созданы Unit - тесты на проверку вызова viewDidLoad, showLogoutAlert, установки аватара, попадают ли переданные параметры в свойства Profile

--- ImageFeedTests/ProfileViewTests.swift
+++ ImageFeedTests/ProfileViewTests.swift
@@ -14,19 +14,57 @@ final class ProfileViewTests: XCTestCase {
         let presenter = ProfileViewPresenterSpy()
         viewController.presenter = presenter
         
-         _ = viewController.view
-
-         XCTAssertTrue(presenter.viewDidLoadCalled)
+        viewController.loadViewIfNeeded()
+        
+        XCTAssertTrue(presenter.viewDidLoadCalled)
     }
     
     func testPresenterCallsShowLogoutAlert() {
         let viewSpy = ProfileViewControllerSpy()
         let presenter = ProfileViewPresenter()
         presenter.view = viewSpy
-
+        
         presenter.didTapLogout()
-
+        
         XCTAssertTrue(viewSpy.showLogoutAlertCalled)
+    }
+    
+    func testProfileViewControllerUpdateAvatarCalled() {
+        let viewController = ProfileViewControllerSpy()
+        let presenter = ProfileViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        presenter.view?.setAvatar(with: Constants.defaultBaseURL)
+        
+        XCTAssertTrue(viewController.updateAvatarCalled)
+    }
+    
+    func testProfileViewControllerUpdateProfile() {
+        let viewController = ProfileViewControllerSpy()
+        let presenter = ProfileViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        presenter.view?.setProfile(name: "Test Name", nickname: "Test Nickname", description: "Test Description")
+        
+        XCTAssertTrue(viewController.isUpdateProfile)
+    }
+    
+    func testProfileViewControllerSetDataProfile() {
+        let viewController = ProfileViewControllerSpy()
+        let presenter = ProfileViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        let testName = "Test Name"
+        let testNickname = "Test Nickname"
+        let testDescription = "Test Description"
+        presenter.view?.setProfile(name: testName, nickname: testNickname, description: testDescription)
+        
+        XCTAssertEqual(viewController.name, testName)
+        XCTAssertEqual(viewController.nickname, testNickname)
+        XCTAssertEqual(viewController.description, testDescription)
     }
 }
 
@@ -47,12 +85,14 @@ final class ProfileViewControllerSpy: ProfileViewControllerProtocol {
     
     var updateAvatarCalled = false
     var showLogoutAlertCalled = false
+    var isUpdateProfile = false
     
     var name: String?
     var nickname: String?
     var description: String?
     
     func setProfile(name: String, nickname: String, description: String) {
+        isUpdateProfile = true
         self.name = name
         self.nickname = nickname
         self.description = description
